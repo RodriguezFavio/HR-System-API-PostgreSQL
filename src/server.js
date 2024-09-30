09dad53cb733fb4cb464025694432e1a0da3ffca
RodriguezFavio <favio.rodriguez.flores@gmail.com>
2024-09-30 18:00:32 -0300

    Refactor server.js: replaced lepayaCourseRouter with coursesRoutes, removed CORS middleware

--- src/server.js
+++ src/server.js
@@ -1,22 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const lepayaCourseRouter = require('./routes/lepayaCourses');
+const coursesRoutes = require('./routes/courses');
 const { errorMiddleware } = require('./middleware/error');
 const app = express();
 
 app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, PUT, PATCH, DELETE'
-  );
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
-
-app.use('/api', lepayaCourseRouter);
+app.use('/api', coursesRoutes);
 
 app.use(errorMiddleware);
 
